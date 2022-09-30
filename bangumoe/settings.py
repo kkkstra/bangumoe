f0ed5ece52436f85572287e2d022797eb0eb5269
patricklai46 <patrick_0406@outlook.com>
2022-10-01 00:33:17 +0800

    Init django

--- bangumoe/settings.py
+++ bangumoe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bangumoes.apps.BangumoesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ bangumoes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BangumoesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bangumoes'
